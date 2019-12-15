1a7182431a59361aea04802106ac63fba71f1634
Andrew Nevero <andrew.nevero@outlook.com>
2019-12-15 12:41:23 +0300

    Prepare the solution to be released

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Computational methods of linear algebra
 
-This repository contains the solutions of the laboratory work offered as
+This repository contains the solutions of the laboratory works offered as
 the part of the "Computational methods of algebra" course at the Faculty of
 Applied Mathematics and Computer Science (BSU, 2019).
 
@@ -13,22 +13,38 @@ non-singular matrix.
 * LDL decomposition for square symmetric matrices.
 * Solving the systems of the linear equations using TLU / LDL decomposition.
 * Counting the inverse matrix using TLU decomposition.
+* Searching for the eigenvalues and eigenvectors with help of several
+algorithms (QR algorithm, Power iteration algorithm, Danilevsky algorithm).
 
 Some algorithms for the special types of matrices are implemented, for
 example, an optimized version of the TLU decomposition algorithm for the
 almost triangular matrices or an algorithm for solving the systems of
 linear equations with the tridiagonal matrices.
 
-C++ multithreading features are used inside some algorithms (to efficiently
-use all the computer resources). [ThreadPool library by Google](
-https://github.com/google/or-tools/blob/v7.4/ortools/base/threadpool.h) is
-sometimes responsible for managing the threads.
+Template Polynomial class is implemented to work with the characteristic
+polynomials of the matrices. It includes arithmetic and differentiation
+operators as well as some methods to find the real roots of the polynomial
+(Newton and bisection algorithms are used for that).
+
+C++11 multithreading features are used inside some algorithms.
+[ThreadPool library by Google](
+https://github.com/google/or-tools/blob/v7.4/ortools/base/threadpool.h)
+is sometimes responsible for managing the threads.
 
 More detailed descriptions of the algorithms can be found inside source
 files (as comments) and inside the reports, which are available in the form
 of Jupyter Notebooks (the Russian language is used there).
 
-Note: the project was created and tested on Linux-based systems. There are no
+## Notes
+
+* The project was created and tested on Linux-based systems. There are no
 guarantees that it will work correctly in Windows (in particular,
 multithreading features may work incorrectly when using the MinGW compiler 
 on Windows).
+
+* Some C++2a features are used inside the project (for example, concepts), so
+you need some modern compiler to build it. clang++-10 was used to create
+this solution (with CLion IDE, which supports C++2a concepts feature), so
+it's recommended to work with it (but other compilers can still be used, if
+they support necessary features).
+
--- main.cpp
+++ main.cpp
@@ -4,100 +4,10 @@
 #include "Tests/Tests.h"
 #include "Utils/DataOutput.h"
 
-#include "Tasks/Task8.h"
-
 using namespace matrix;
 using namespace polynomial;
 
 int main() {
-//  std::vector<std::vector<double>> v = {{5, 3, -18, -3},
-//                                        {1, -82.7, 101.7, 81.7},
-//                                        {4, -146.2, 168.2, 146.2},
-//                                        {-8, -8, 0, 7}};
-
-//  std::vector<std::vector<double>> v = {{90, 72, -36},
-//                                        {-91, -73, 36},
-//                                        {34, 34, -18}};
-
-//  std::vector<std::vector<double>> v = {{7, -12, -2},
-//                                        {3, -4, 0},
-//                                        {-2, 0, -2}};
-
-//  std::vector<std::vector<double>> v = {{-2, 2, 4},
-//                                        {2, 3, -1},
-//                                        {4, -1, 1}};
-
-//  std::vector<std::vector<double>> v = {{2, 2, 4},
-//                                        {2, 3, -1},
-//                                        {4, -1, 1}};
-
-//  std::vector<std::vector<double>> v = {{1, 5, 10, 15},
-//                                        {0, 54, 45, 32},
-//                                        {0, 42, 42, 1},
-//                                        {0, 0, 0, 4}};
-
-//  std::vector<std::vector<double>> v = {{25, 20, -10},
-//                                        {-26, -21, 10},
-//                                        {8, 8, -5}};
-
-//  std::fstream in("../input_files/task6_input1");
-//  double a;
-//  in >> a;
-//  std::vector<std::vector<double>> v(a, std::vector<double>(a));
-//  for (int i = 0; i < a; ++i) {
-//    for (int j = 0; j < a; ++j) {
-//      in >> v[i][j];
-//    }
-//  }
-//
-//  Matrix m(v);
-//  m.CountFrobeniusMatrix();
-//  m.CountCharacteristicPolynomial();
-//  m.FindCharacteristicPolynomialRoots();
-//  std::cout << m.GetFrobeniusMatrix() << std::endl;
-//
-//  for (const auto& item : m.GetEigenvectorsFromFrobeniusMatrix()) {
-//    std::cout << item.value << std::endl << item.vector << std::endl;
-//  }
-
-//  Matrix m(v);
-//  m.RunQrAlgorithm(0.00000001);
-//  auto t = m.GetEigenvectorsFromHessenbergMatrix(0.01);
-//  for (const auto& item : t) {
-//    std::cout << item.value << std::endl << item.vector << std::endl;
-//  }
-//  std::cout << m.GetUpperHessenbergMatrix();
-
-//  Matrix m(v);
-//  std::cout << m << std::endl;
-//  auto t = m.GetPowerIterationResults(0.000001);
-//  for (auto& item : t) {
-//    std::cout << item.value << std::endl << item.vector << std::endl;
-//  }
-
-  std::cout << matrix_tasks::Task8<double>::TimeForMatrix(100, 0.00000000000001, 100000);
-
-//  auto first_result = matrix_tasks::Task8<double>::RandomInput(70, 0.0001);
-//  std::cout << "first" << std::endl;
-//  for (auto item : first_result) {
-//    std::cout << item << ", ";
-//  }
-//  std::cout << std::endl;
-//
-//  auto second_result = matrix_tasks::Task8<double>::RandomInput(70, 0.000001);
-//  std::cout << "second" << std::endl;
-//  for (auto item : second_result) {
-//    std::cout << item << ", ";
-//  }
-//  std::cout << std::endl;
-//
-//
-//  auto third_result = matrix_tasks::Task8<double>::RandomInput(70, 0.00000001);
-//  std::cout << "third" << std::endl;
-//  for (auto item : third_result) {
-//    std::cout << item << ", ";
-//  }
-//  std::cout << std::endl;
 
   return 0;
 }
